266a712966b074856c24911ba378c78d70887004
phoenixit99 <hoanguit@gmail.com>
2025-01-02 13:38:55 +0700

    Update build

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'themes/theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
