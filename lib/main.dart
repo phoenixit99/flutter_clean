8665316353a35866f10f0a5c52b6677d607ca68c
phoenixit99 <hoanguit@gmail.com>
2025-01-17 13:16:26 +0700

    update: update workflow

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,8 @@ void main() {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String message = '';
 
   @override
   Widget build(BuildContext context) {
@@ -32,28 +25,70 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(message),
-            const WalletScreen(),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            final file = File('lib/assets/pactus/pactus-cli_1.6.4_darwin_arm64.tar.gz');
-            setState(() {
-              if (file.existsSync()) {
-                message = 'Pactus CLI file exists! File size: ${file.lengthSync()} bytes';
-              } else {
-                message = 'Pactus CLI file does not exist. Expected path: ${file.absolute.path}';
-              }
-            });
-          },
-          child: const Icon(Icons.check),
-          tooltip: 'Check Pactus CLI',
-        ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String message = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const WalletScreen(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final file = File('lib/assets/pactus/pactus-cli_1.6.4_darwin_arm64.tar.gz');
+          setState(() {
+            if (file.existsSync()) {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('File Check'),
+                    content: Text('Pactus CLI file exists! File size: ${file.lengthSync()} bytes'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: const Text('OK'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            } else {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('File Check'),
+                    content: Text('Pactus CLI file does not exist. Expected path: ${file.absolute.path}'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: const Text('OK'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            }
+          });
+        },
+        child: const Icon(Icons.check),
+        tooltip: 'Check Pactus CLI',
       ),
     );
   }
